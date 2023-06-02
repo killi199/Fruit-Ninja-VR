35c50d4d233d046b7ceac9e03ffcac48cec05499
Lukas Görtz <lukehd11@gmail.com>
2023-06-02 20:53:29 +0200

    Add Spawner Object
    Spawner Object (Plane) spawns given Prefabs under itself in a given interval.

---
+++ Assets/ObjectSpawner.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject[] prefabs; // Array für die Prefabs der zu erstellenden Objekte
+    public float spawnInterval = 1.0f; // Intervall zwischen den Spawns
+    public Vector3 spawnOffset = Vector3.zero; // Offset zur Position des Spawners
+    public Vector3 randomRotationRange = new Vector3(0f, 360f, 0f); // Bereich für die zufällige Rotation
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnObjects());
+    }
+
+    IEnumerator SpawnObjects()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnInterval);
+
+            Vector3 spawnPosition = GetRandomSpawnPosition();
+            Quaternion spawnRotation = Quaternion.Euler(RandomRotation());
+            GameObject randomPrefab = GetRandomPrefab();
+
+            _ = Instantiate(randomPrefab, spawnPosition, spawnRotation);
+        }
+    }
+
+    Vector3 GetRandomSpawnPosition()
+    {
+        Renderer spawnerRenderer = GetComponent<Renderer>();
+        Bounds spawnerBounds = spawnerRenderer.bounds;
+
+        float x = Random.Range(-spawnerBounds.extents.x, spawnerBounds.extents.x);
+        float z = Random.Range(-spawnerBounds.extents.z, spawnerBounds.extents.z);
+
+        Vector3 spawnPosition = transform.position + spawnOffset + new Vector3(x, 0f, z);
+
+        return spawnPosition;
+    }
+
+    GameObject GetRandomPrefab()
+    {
+        int randomIndex = Random.Range(0, prefabs.Length);
+        return prefabs[randomIndex];
+    }
+
+    Vector3 RandomRotation()
+    {
+        float x = Random.Range(randomRotationRange.x, randomRotationRange.y);
+        float y = Random.Range(randomRotationRange.x, randomRotationRange.y);
+        float z = Random.Range(randomRotationRange.x, randomRotationRange.y);
+
+        return new Vector3(x, y, z);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
