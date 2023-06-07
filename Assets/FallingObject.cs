d1541027a4aa3e4203731240d1f175a505f83ef1
Lukas Görtz <lukehd11@gmail.com>
2023-06-07 16:36:33 +0200

    Früchte fallen und löschen
    Lässt die Früchte fallen und löscht sie, wenn gegebene Tiefe erreicht ist

---
+++ Assets/FallingObject.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObject : MonoBehaviour
+{
+    public float fallSpeed = 3f; // Geschwindigkeit, mit der das Objekt fällt
+    public float deleteUnderY = -5f;
+
+    private void Update()
+    {
+        // Bewege das Objekt nach unten entlang der globalen Y-Achse mit der festgelegten Geschwindigkeit
+        transform.Translate(Vector3.down * fallSpeed * Time.deltaTime, Space.World);
+
+        // Überprüfe die Y-Koordinate des Objekts
+        if (transform.position.y < deleteUnderY)
+        {
+            // Lösche das Objekt
+            Destroy(gameObject);
+        }
+    }
+}
